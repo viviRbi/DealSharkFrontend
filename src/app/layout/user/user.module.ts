72d5029ebfcd253f276bc88db60241728d3a416e
Quinn Slattery <quinn.lee.slatt@gmail.com>
2021-03-26 20:23:08 -0500

    Checkout component completed(ish)

--- src/app/layout/user/user.module.ts
+++ src/app/layout/user/user.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RoutingUserModule } from '../../routing-module/routing-user.module';
 /*import { ProfileComponent } from './profile/profile.component';
 import { OwnedGameComponent } from './owned-game/owned-game.component';
